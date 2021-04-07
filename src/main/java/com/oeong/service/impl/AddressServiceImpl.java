b14eae0ebed1b4adfebb60fa886ba36c025740da
Hongchenglong <oeong@foxmail.com>
2021-04-07 21:29:49 +0800

    AddressService业务接口 创建/更新地址

--- src/main/java/com/oeong/service/impl/AddressServiceImpl.java
+++ src/main/java/com/oeong/service/impl/AddressServiceImpl.java
@@ -1,5 +1,7 @@
 package com.oeong.service.impl;
 
+import com.oeong.entity.BuyerAddress;
+import com.oeong.form.AddressForm;
 import com.oeong.repository.BuyerAddressRepository;
 import com.oeong.service.AddressService;
 import com.oeong.vo.AddressVO;
@@ -26,5 +28,26 @@ public class AddressServiceImpl implements AddressService {
                         e.getBuyerAddress()
                 )).collect(Collectors.toList());
         return list;
+    }
+
+    @Override
+    public void saveOrUpdate(AddressForm addressForm) {
+        BuyerAddress buyerAddress;
+        if (addressForm.getId() == null) {
+            buyerAddress = new BuyerAddress();
+        } else {
+            buyerAddress = buyerAddressRepository.findById(addressForm.getId()).get();
+        }
+        buyerAddress.setBuyerName(addressForm.getName());
+        buyerAddress.setBuyerPhone(addressForm.getTel());
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(addressForm.getProvince())
+                .append(addressForm.getCity())
+                .append(addressForm.getCounty())
+                .append(addressForm.getAddressDetail());
+        buyerAddress.setBuyerAddress(stringBuffer.toString());
+        buyerAddress.setAreaCode(addressForm.getAreaCode());
+
+        buyerAddressRepository.save(buyerAddress);
     }
 }
--- src/test/java/com/oeong/service/impl/AddressServiceImplTest.java
+++ src/test/java/com/oeong/service/impl/AddressServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.oeong.service.impl;
 
+import com.oeong.form.AddressForm;
 import com.oeong.service.AddressService;
 import com.oeong.vo.AddressVO;
 import org.junit.jupiter.api.Test;
@@ -14,13 +15,27 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class AddressServiceImplTest {
 
-    @Autowired // 忘记在Impl加@Service
+    @Autowired // 忘记在Impl加@Service，报错
     private AddressService addressService;
 
     @Test
     void findAll() {
         List<AddressVO> list = addressService.findAll();
         int id = 0;
+    }
+
+    @Test
+    void saveOrUpdate() {
+        AddressForm addressForm = new AddressForm();
+        addressForm.setId(35);
+        addressForm.setName("李四");
+        addressForm.setTel("13612344321");
+        addressForm.setProvince("北京市");
+        addressForm.setCity("北京市");
+        addressForm.setCounty("东城区");
+        addressForm.setAreaCode("110101");
+        addressForm.setAddressDetail("168号606室");
+        addressService.saveOrUpdate(addressForm);
     }
 
 }
