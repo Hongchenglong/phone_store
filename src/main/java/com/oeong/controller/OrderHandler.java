5b41123aaa529f0662b0542c9467e530ee2d9f92
Hongchenglong <oeong@foxmail.com>
2021-04-11 11:10:23 +0800

    OrderHandler控制层

---
+++ src/main/java/com/oeong/controller/OrderHandler.java
@@ -0,0 +1,72 @@
+package com.oeong.controller;
+
+import com.oeong.dto.OrderDTO;
+import com.oeong.exception.PhoneException;
+import com.oeong.form.OrderForm;
+import com.oeong.service.OrderService;
+import com.oeong.util.ResultVOUtil;
+import com.oeong.vo.ResultVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/order")
+@Slf4j
+public class OrderHandler {
+
+    @Autowired
+    private OrderService orderService;
+
+    // 创建用post, 更新用Put
+    @PostMapping("/create")
+    public ResultVO create(@Valid @RequestBody OrderForm orderForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            log.error("【创建订单】参数错误,orderForm={}", orderForm);
+            throw new PhoneException(bindingResult.getFieldError().getDefaultMessage());
+        }
+
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setBuyerName(orderForm.getName());
+        orderDTO.setBuyerPhone(orderForm.getTel());
+        orderDTO.setBuyerAddress(orderForm.getAddress());
+        orderDTO.setSpecsId(orderForm.getSpecsId());
+        orderDTO.setPhoneQuantity(orderForm.getQuantity());
+
+        OrderDTO result = orderService.create(orderDTO);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("orderId", result.getOrderId());
+
+        return ResultVOUtil.success(map);
+    }
+
+    @GetMapping("/detail/{orderId}")
+    public ResultVO findOrderDetail(
+            @PathVariable("orderId") String orderId) {
+        return ResultVOUtil.success(orderService.findOrderDetailByOrderId(orderId));
+    }
+
+    @PutMapping("/pay/{orderId}")
+    public ResultVO pay(
+            @PathVariable("orderId") String orderId) {
+        Map<String, String> map = new HashMap<>();
+        map.put("orderId", orderService.pay(orderId));
+        return ResultVOUtil.success(map);
+    }
+
+}
+/*
+{
+  "name": "张三",
+  "tel": "13678787878",
+  "address": "广东省深圳市罗湖区科技路123号456室",
+  "specsId": 1,
+  "quantity": 1
+}
+ */
