1928a1c6975fa102e23b86ecdab7725e57b993a2
Hongchenglong <oeong@foxmail.com>
2021-04-12 10:59:24 +0800

    rm src

--- src/main/java/com/oeong/repository/BuyerAddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oeong.repository;
-
-import com.oeong.entity.BuyerAddress;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BuyerAddressRepository extends JpaRepository<BuyerAddress, Integer> {
-}
